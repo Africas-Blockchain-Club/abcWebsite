42c251d86481e22969d65e1c4d779d4b6be10284
sandileSiwundla <sandsmthembu2@gmail.com>
2025-04-19 04:44:52 +0200

    Added Elephant

--- abc/app/page.tsx
+++ abc/app/page.tsx
@@ -2,7 +2,8 @@ import Image from "next/image";
 import { navItems } from "@/data";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import ContactUs from "@/components/ui/contactUs";
-import Carousel from "@/components/ui/Carousel"; // Import the Carousel component
+import Carousel from "@/components/ui/Carousel"; 
+import SecretElephant from "@/components/secretElephant"; 
 
 export default function Home() {
   return (
@@ -16,14 +17,19 @@ export default function Home() {
       >
         <FloatingNav navItems={navItems} />
 
-        {/* Add spacing if needed */}
         <div className="pt-32 px-8">
           <ContactUs />
         </div>
 
         <div className="pt-32 px-8">
-          <Carousel /> {/* Call the Carousel component here */}
+          <Carousel />
         </div>
+
+        {/* <div className="pt-32 px-8">
+          <SecretElephant />
+        </div> */}
+
+        
 
         {Array(100)
           .fill(null)
