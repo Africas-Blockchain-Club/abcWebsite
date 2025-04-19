4cbee6872628a7c34d61a583f9159052df6812a1
sandileSiwundla <sandsmthembu2@gmail.com>
2025-04-19 19:23:20 +0200

    Have inverted images

--- abc/components/About.tsx
+++ abc/components/About.tsx
@@ -20,32 +20,38 @@ export default function FullscreenCarousel() {
 
       {/* Islands Section */}
       <div className="flex justify-center gap-8 px-8">
-        {aboutText.map((item, index) => (
-          <div
-            key={index}
-            className="min-w-[400px] max-w-[600px] h-full flex flex-col items-center justify-center text-white snap-start pt-16 pb-10 rounded-2xl overflow-hidden border-4 border-white"
-          >
-            <div className="mb-6 rounded-full overflow-hidden">
-  <Image
-    src={aboutImages[index].src}
-    alt={aboutImages[index].alt}
-    width={aboutImages[index].width}
-    height={aboutImages[index].height}
-    className="rounded-full"
-  />
-</div>
+        {aboutText.map((item, index) => {
+          const [isHovered, setIsHovered] = React.useState(false);
 
-            <dt className="text-5xl font-Plush text-white text-center">
-              {item.title1}
-            </dt>
-            <dt className="text-5xl font-Plush text-white text-center mt-4">
-              {item.title2}
-            </dt>
-            <dd className="mt-6 text-xl font-Plush text-white text-center max-w-xl px-4">
-              {item.description}
-            </dd>
-          </div>
-        ))}
+          return (
+            <div
+              key={index}
+              className="min-w-[400px] max-w-[600px] h-full flex flex-col items-center justify-center text-white snap-start pt-16 pb-10 rounded-2xl overflow-hidden border-2 border-blue-500 shadow-[0_0_30px_10px_rgba(59,130,246,0.5)]"
+              onMouseEnter={() => setIsHovered(true)}
+              onMouseLeave={() => setIsHovered(false)}
+            >
+              <div className="mb-6 rounded-full overflow-hidden">
+                <Image
+                  src={isHovered ? aboutImages[index].inverted : aboutImages[index].src}
+                  alt={aboutImages[index].alt}
+                  width={aboutImages[index].width}
+                  height={aboutImages[index].height}
+                  className="rounded-full transition duration-300 ease-in-out"
+                />
+              </div>
+
+              <dt className="text-5xl font-Plush text-white text-center">
+                {item.title1}
+              </dt>
+              <dt className="text-5xl font-Plush text-white text-center mt-4">
+                {item.title2}
+              </dt>
+              <dd className="mt-6 text-xl font-Plush text-white text-center max-w-xl px-4">
+                {item.description}
+              </dd>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
