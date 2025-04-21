510fceb9b2a74a3eaf25b9842fa45932d3fa33e8
sandileSiwundla <sandsmthembu2@gmail.com>
2025-04-21 16:21:59 +0200

    Fixed up About.tsx

--- abc/components/About.tsx
+++ abc/components/About.tsx
@@ -8,25 +8,24 @@ export default function FullscreenCarousel() {
   return (
     <div className="min-h-screen">
       {/* Header Section */}
-      <div className="flex flex-col items-center justify-center text-center py-8">
-        <h2 className="text-3xl font-extrabold text-white sm:text-4xl">
+      <div className="flex flex-col items-center justify-center text-center py-18">
+        <h2 className="text-6xl font-extrabold text-white sm:text-6xl py-4">
           About Africa's Blockchain Club
         </h2>
-        <p className="mt-4 max-w-2xl text-xl text-white lg:mx-auto">
+        <p className="mt-5 max-w-4xl text-xl text-white lg:mx-auto text-6l sm:text-6l">
           We are a community-driven organization dedicated to advancing Web3 technology and fostering innovation in
           the blockchain space across Africa.
         </p>
       </div>
 
       {/* Islands Section */}
-      <div className="flex justify-center gap-8 px-8">
+      <div className="flex justify-center gap-28 px-8">
         {aboutText.map((item, index) => {
-          const [isHovered, setIsHovered] = React.useState(false);
 
           return (
             <div
   key={index}
-  className="min-w-[400px] max-w-[600px] h-full flex flex-col items-center justify-center snap-start pt-16 pb-10 rounded-2xl overflow-hidden border-2 border-yellow-500 shadow-[0_0_30px_10px_rgba(234,179,8,0.75)] bg-transparent text-white"
+  className="min-w-[400px] max-w-[550px] h-full flex flex-col items-center justify-center snap-start pt-16 pb-10 rounded-2xl overflow-hidden border-2 border-yellow-500 shadow-[0_0_30px_10px_rgba(234,179,8,0.75)] bg-transparent text-white"
 >
   <div className="mb-6  overflow-hidden">
     <Image
