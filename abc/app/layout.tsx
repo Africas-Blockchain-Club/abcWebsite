fdedc61ac579938bfb013a3fadf9f5365461c1c6
sandileSiwundla <sandsmthembu2@gmail.com>
2025-10-08 21:16:55 +0200

    Wokring on mobile view projects

--- abc/app/layout.tsx
+++ abc/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <FloatingNav navItems={navItems} />
-        <FloatingImage />
+        {/* <FloatingImage /> */}
         {children}
       </body>
     </html>
